3e5b128cfe6f03ebaab5cb357933c4eb7c9b6f64
thiagocontelli <thiagocontellid@gmail.com>
2023-05-24 09:41:40 -0300

    refactor: change findViewById to view binding

--- app/src/main/java/com/thiagocontelli/infinitescrollexample/HomeFragment.kt
+++ app/src/main/java/com/thiagocontelli/infinitescrollexample/HomeFragment.kt
@@ -6,17 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.thiagocontelli.infinitescrollexample.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
     private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/thiagocontelli/infinitescrollexample/MainActivity.kt
+++ app/src/main/java/com/thiagocontelli/infinitescrollexample/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.thiagocontelli.infinitescrollexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.thiagocontelli.infinitescrollexample.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
